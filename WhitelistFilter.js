c38dac7abaa690874a0888e9e2dca6c970f58340
Oluwafemi <ojurifemi132@gmal.com>
2025-03-08 00:08:47 +0100

    Initial commit

---
+++ WhitelistFilter.js
@@ -0,0 +1,32 @@
+const fs = require("fs").promises;
+
+const whitelist = new Set();
+
+class WhitelistFilter {
+  constructor() {
+  }
+
+  async fetchWhitelist(path) {
+    try {
+      const data = await fs.readFile(path, { encoding: 'utf8' });
+
+      data.split("\n").forEach((line) => {
+        whitelist.add(line.trim());
+      });
+
+      return whitelist;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  isIpWhitelisted(ip) {
+    return whitelist.has(ip)
+  }
+
+  readWhitelist() {
+    return whitelist;
+  }
+}
+
+module.exports = WhitelistFilter;
---
+++ index.js
@@ -0,0 +1,30 @@
+const WhitelistFilter = require("./WhitelistFilter.js");
+
+const args = process.argv[2];
+
+let wlFilter = new WhitelistFilter();
+
+(async () => {
+  try {
+    await wlFilter.fetchWhitelist(args);
+  } catch (err) {
+    console.error("Error:", err);
+  }
+
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  rl.on("line", (line) => {
+    if (wlFilter.isIpWhitelisted(line.trim())) {
+      console.log(`✅ ${line.trim()}`);
+    } else {
+        console.log('❌ Not Whitelisted')
+    }
+  });
+
+})();
